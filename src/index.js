454b3eef13dacb31cb5961e54fc504837f9c6bdb
Anuj Decoded <nakuldhule301@gmail.com>
2026-05-30 13:08:26 +0530

    Utils files added

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path: "./.env"
 });
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT, () =>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.error("Error connecting to database:", err);
+});
